249f546d1d90889f4d39750d23137b4764475e46
Alexandra DENOS <azekool123@gmail.com>
2021-11-05 11:55:30 +0100

    ajout du logo derichebourg à la place du logo Vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/bison-rouge.png" alt="bison rouge" id="logoDerichebourg">
     <IndexMenu/>
     <Footer/>
   </div>
@@ -25,5 +25,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#logoDerichebourg {
+  height: 250px;
+  width: auto;
+  margin-bottom: 25px;
 }
 </style>
